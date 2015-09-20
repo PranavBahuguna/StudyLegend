7b48e1f5dc22f1b0644dad85ea30ca84c2f9367d
Pranav Bahuguna <pranav.bahuguna@gmail.com>
2015-09-20 13:04:16 +0100

    Enabled menu to be dynamically generated from xml file.

---
+++ scripts.js
@@ -0,0 +1,49 @@
+// Loads an XML file and returns the XML data.
+function loadXMLDoc(filename)
+{
+    if (window.XMLHttpRequest) {
+      xhttp = new XMLHttpRequest();
+    } else { // code for IE5 and IE6
+        xhttp = new ActiveXObject("Microsoft.XMLHTTP");
+    }
+    xhttp.open("GET", filename, false);
+    xhttp.send();
+    return xhttp.responseXML;
+}
+
+// Generates a traversible menu of links given an appropriate xml file.
+function generateMenuFromFile(htmlContainer, xmlFile) {
+    links = loadXMLDoc(xmlFile);
+    contents = links.getElementsByTagName("contents")[0];
+    generateMenu(htmlContainer, contents);
+}
+
+// Recursive function that generates
+function generateMenu(htmlContainer, xmlMenu) {
+    // Adds a list to the htmlContainer and sets the htmlContainer to be the
+    // newly created list.
+    htmlContainer.innerHTML += "<ul></ul>";
+    htmlContainer = htmlContainer.getElementsByTagName('ul')[0];
+
+    // Obtains all menus and items from the xmlMenu.
+    var menus = []
+    var items = []
+    for (i = 0; i < xmlMenu.childNodes.length; i++) {
+        if (xmlMenu.childNodes[i].nodeName == "menu") {
+            menus.push(xmlMenu.childNodes[i]);
+        } else if (xmlMenu.childNodes[i].nodeName == "item") {
+            items.push(xmlMenu.childNodes[i]);
+        }
+    }
+
+    // Writes each menu in the menus array into the htmlContainer and calls
+    // generateMenu on the new menu.
+    for (i = 0; i < menus.length; i++) {
+        htmlContainer.innerHTML += '<li><a class="menu collapsed">' + menus[i].childNodes[1].innerHTML + '</a></li>'
+        arguments.callee(htmlContainer.childNodes[0], menus[i].getElementsByTagName('items')[0]);
+    }
+    // Adds remaining items into the htmlContainer.
+    for (i = 0; i < items.length; i++) {
+        htmlContainer.innerHTML += '<li><a class="item">' + items[i].childNodes[1].innerHTML + '</a></li>'
+    }
+}
